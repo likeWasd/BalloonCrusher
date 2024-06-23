566fe95d7b7af928a398d98ca64284f024ec4274
likeWasd <mana.dog0077@gmail.com>
2024-06-23 13:07:05 +0900

    [feat]3体倒した時、Sceneを切り替えられるようにした
    ただ、StageManagerのコードが少し分かりづらい気がする
    Unityのバージョンを2022.3.34F1にした

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
@@ -9,15 +10,26 @@ public class StageManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (killedEnemyCount >= 30)
+        if (killedEnemyCount >= 3)
         {
-
+            killedEnemyCount = 0;
+            stageNumber++;
+            if (SceneManager.GetSceneByName("Stage1").name == "Stage1")
+            {
+                SceneManager.LoadScene("Stage2", LoadSceneMode.Additive);
+                SceneManager.UnloadSceneAsync("Stage1");
+            }
+            else if (SceneManager.GetSceneByName("Stage2").name == "Stage2")
+            {
+                SceneManager.LoadScene("Stage1", LoadSceneMode.Additive);
+                SceneManager.UnloadSceneAsync("Stage2");
+            }
         }
     }
 }
