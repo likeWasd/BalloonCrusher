d5ca29b78c6173c93645b73ee9426788c0c1e519
likeWasd <mana.dog0077@gmail.com>
2024-07-14 13:04:07 +0900

    敵のHPバーを追加した
    ・EmemyController.csのいらないコメントアウトを消した
    ・maxHpという変数を追加した
    ・敵の生成のエラーを解消した
    ・移動にかかる時間が早すぎたので、2.5秒から3.0秒にした
    ・2022.3.37f1にバージョンアップした

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,44 +8,14 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        StartCoroutine(EnemyControlCoroutine());
-        */
         MoveToNewPosition(); // 初回の移動を開始
-        /*
-        Vector3 targetPos = new Vector3(
-            Random.Range(-18.0f, 18.0f),
-            1.0f,
-            Random.Range(-18.0f, 18.0f)
-            );
-        var sequence = DOTween.Sequence();
-        sequence.Append(transform.DOMove(targetPos, Random.Range(1.5f, 2.0f)))
-                .AppendCallback(() =>
-                {
-                    targetPos = new Vector3(
-                    Random.Range(-18.0f, 18.0f),
-                    1.0f,
-                    Random.Range(-18.0f, 18.0f)
-                    );
-                })
-                .Append(transform.DOMove(targetPos, Random.Range(1.5f, 2.0f)))
-                .AppendCallback(() =>
-                {
-                    targetPos = new Vector3(
-                    Random.Range(-18.0f, 18.0f),
-                    1.0f,
-                    Random.Range(-18.0f, 18.0f)
-                    );
-                })
-                .SetLoops(-1, LoopType.Restart);
-        */
     }
     
     void MoveToNewPosition()
     {
         Vector3 targetPos = GetRandomPosition(); // 新しいランダムな位置を取得
-        float moveDuration = Random.Range(1.5f, 2.0f); // 移動にかかる時間
-        transform.DOMove(targetPos, moveDuration).OnComplete(MoveToNewPosition); // 移動が完了したら再度移動を開始
+        float moveDuration = Random.Range(2.5f, 3.0f); // 移動にかかる時間
+        transform.DOMove(targetPos, moveDuration).SetLink(this.gameObject).OnComplete(MoveToNewPosition); // 移動が完了したら再度移動を開始
     }
     
     Vector3 GetRandomPosition() 
@@ -58,21 +28,4 @@ public class EnemyController : MonoBehaviour
     {
 
     }
-
-
-    /*
-    IEnumerator EnemyControlCoroutine()
-    {
-        while (true)
-        {
-            Vector3 targetPos = new Vector3(
-                Random.Range(-18.0f, 18.0f),
-                1.0f,
-                Random.Range(-18.0f, 18.0f)
-                );
-            transform.DOMove(targetPos, Random.Range(1.5f, 2.0f));
-            yield return null;
-        }
-    }
-    */
 }
--- Assets/Scripts/EnemyHPManager.cs
+++ Assets/Scripts/EnemyHPManager.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHPManager : MonoBehaviour
 {
-    public int HP;
+    public int hp;
+    public int maxHp;
     StageManager stageManager;
+    public Canvas canvas;
+    public Slider hpBar;
     // Start is called before the first frame update
     void Start()
     {
         stageManager = GameObject.Find("SystemManager").GetComponent<StageManager>();
-        HP = 5 + (stageManager.stageNumber - 1) * 3;
+        maxHp = 5 + (stageManager.stageNumber - 1) * 3;
+        hp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        canvas.transform.rotation = Camera.main.transform.rotation;
+        hpBar.value = (float)hp / (float)maxHp;
     }
 
     public void GetDamage(int damage)
     {
-        HP -= damage;
-        if (HP == 0)
+        hp -= damage;
+        if (hp == 0)
         {
             // Enemyを消す処理
             Destroy(this.gameObject);
