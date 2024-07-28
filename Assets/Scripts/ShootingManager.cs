460d90f837a819e7b483710c3eaffe06bbdd8052
likeWasd <mana.dog0077@gmail.com>
2024-07-28 13:06:23 +0900

    ShootingManager.csを改良しまくった
    マシンガンの2つのバグを解消した

--- Assets/Scripts/ShootingManager.cs
+++ Assets/Scripts/ShootingManager.cs
@@ -6,6 +6,9 @@ public class ShootingManager : MonoBehaviour
 {
     StageManager stageManager;
     Coroutine stopMachineGun;
+    GameObject hitObject;
+    int weaponType;
+    // int 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,43 +18,67 @@ public class ShootingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 40.0f))
+        if (stageManager.stageNumber <= 0)
         {
-            GameObject hitObject = hit.collider.gameObject;
-            // 攻撃の処理
-            if (Input.GetMouseButtonDown(0))
+            weaponType = 1;
+        }
+        else
+        {
+            weaponType = 2;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (weaponType == 1)
             {
-                // Enemyに当たったときの処理
-                if (hitObject.CompareTag("Enemy"))
+                hitObject = GetRaycastHitObject();
+                if (hitObject != null)
                 {
-                    if (stageManager.stageNumber <= 0)
+                    if (hitObject.CompareTag("Enemy"))
                     {
                         EnemyHPManager enemyHP = hitObject.GetComponent<EnemyHPManager>();
                         enemyHP.GetDamage(1);
                     }
-                    else
-                    {
-                        EnemyHPManager enemyHP = hitObject.GetComponent<EnemyHPManager>();
-                        stopMachineGun = StartCoroutine(machineGun(enemyHP));
-                        // StartCoroutine(machineGun(enemyHP));
-                    }
                 }
             }
-            if (Input.GetMouseButtonUp(0) && hitObject.CompareTag("Enemy") && !(stageManager.stageNumber <= 0))
+            if (weaponType == 2)
+            {
+                stopMachineGun = StartCoroutine(machineGun());
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (stopMachineGun != null)
             {
                 StopCoroutine(stopMachineGun);
             }
         }
     }
 
-    IEnumerator machineGun(EnemyHPManager enemyHP)
+    GameObject GetRaycastHitObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 40.0f))
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+
+    IEnumerator machineGun()
     {
         while (true)
         {
+            hitObject = GetRaycastHitObject();
+            if (hitObject != null)
+            {
+                if (hitObject.CompareTag("Enemy"))
+                {
+                    EnemyHPManager enemyHP = hitObject.GetComponent<EnemyHPManager>();
+                    enemyHP.GetDamage(1);
+                }
+            }
             yield return new WaitForSeconds(0.1f);
-            enemyHP.GetDamage(1);
         }
     }
 }
